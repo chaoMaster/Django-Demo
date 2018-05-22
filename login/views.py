58c2d5e081c2db213220674817155e1b65a6da48
chaochao <chaochao@fake.local>
2018-05-22 17:51:32 +0800

    first commit

---
+++ login/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import HttpResponse
+
+def loginPage(request):
+
+
+    return render(request, "login.html")
